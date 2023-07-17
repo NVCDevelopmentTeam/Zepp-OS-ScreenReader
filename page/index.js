50e4d898b610a10175572152e4240377d84027f8
NVCDevelopmentTeam <82768878+NVCDevelopmentTeam@users.noreply.github.com>
2023-07-17 21:09:52 +0700

    update code

--- page/index.js
+++ page/index.js
@@ -1,7 +1,19 @@
-import { gettext } from 'i18n'
+import { gettext } from 'i18n';
 
 Page({
   build() {
-    console.log(gettext('example'))
+    console.log(gettext('example'));
+
+    // Render the main home screen UI here
+    const homeScreen = document.createElement('div');
+    homeScreen.innerHTML = `
+      <h1>${gettext('Welcome to the Screen Reader')}</h1>
+      <p>${gettext('This is the main home screen of the Screen Reader app.')}</p>
+      <p>${gettext('Use the navigation buttons to browse through different sections.')}</p>
+      <button>${gettext('Start Reading')}</button>
+    `;
+
+    // Append the home screen UI to the document body
+    document.body.appendChild(homeScreen);
   }
-})
+});
