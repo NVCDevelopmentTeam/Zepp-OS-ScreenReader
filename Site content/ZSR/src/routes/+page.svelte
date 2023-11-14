f46460eac27e6b9544ae85a2160e93aa8a44e403
NVCDevelopmentTeam <82768878+NVCDevelopmentTeam@users.noreply.github.com>
2023-11-14 22:32:45 +0700

    create site content

---
+++ Site content/ZSR/src/routes/+page.svelte
@@ -0,0 +1,2 @@
+<h1> Hello</h1>
+<p> Currently, the website is under construction. Please come back at another time. <br> For more information about the project, please contact me via email quang.hnm.hd.2k@gmail.com, sincerely thank you for your trust and always accompanying the project. Hopefully, one day soon we can continue to walk together. See you tomorrow. Wish you have a happy working day. Goodbye</p>
