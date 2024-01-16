d4d251a1e26f11ebed146ddd7d7da69057df02ef
NVCDevelopmentTeam <82768878+NVCDevelopmentTeam@users.noreply.github.com>
2024-01-17 06:22:00 +0700

    update code

--- site/www/vite.config.js
+++ site/www/vite.config.js
@@ -1,12 +1,13 @@
-import { defineConfig } from 'vite'
 import { sveltekit } from '@sveltejs/kit/vite'
 
-export default defineConfig({
-  plugins: [sveltekit()],
-  // allows vite access to ./posts
-  server: {
-    fs: {
-      allow: ['./']
-    }
-  }
-})
+/** @type {import('vite').UserConfig} */
+const config = {
+	plugins: [sveltekit()],
+	server: {
+		fs: {
+			allow: ['.']
+		}
+	}
+};
+
+export default config
