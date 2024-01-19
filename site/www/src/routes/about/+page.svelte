a56d1334fab8997e7cf1ab5e2b52299a44677d70
NVCDevelopmentTeam <82768878+NVCDevelopmentTeam@users.noreply.github.com>
2024-01-19 10:19:10 +0700

    update code

--- site/www/src/routes/about/+page.svelte
+++ site/www/src/routes/about/+page.svelte
@@ -1,6 +1,6 @@
 <svelte:head>
 	<title>About</title>
-	<meta name="description" content="About this app" />
+	<meta name="description" content="About website />
 </svelte:head>
 
 <div class="text-column">
