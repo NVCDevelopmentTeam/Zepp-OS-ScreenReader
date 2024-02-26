a53f76575324fe4811fa0c4f9230d4db84a2de01
NVCDevelopmentTeam <82768878+NVCDevelopmentTeam@users.noreply.github.com>
2024-02-26 07:54:20 +0700

    update code

--- site/htdocs/src/routes/+layout.js
+++ site/htdocs/src/routes/+layout.js
@@ -1,2 +1,6 @@
 // remove this if you do not want your pages to be prerendered
 export const prerender = true
+import { inject } from '@vercel/analytics'
+import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
+
+injectSpeedInsights();
