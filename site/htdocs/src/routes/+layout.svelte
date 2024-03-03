13301be43b2894fd3b9326b6a8aa7a8005ed9d9c
NVCDevelopmentTeam <82768878+NVCDevelopmentTeam@users.noreply.github.com>
2024-03-03 21:26:21 +0700

    update code

--- site/htdocs/src/routes/+layout.svelte
+++ site/htdocs/src/routes/+layout.svelte
@@ -1,8 +1,9 @@
 <script>
+  import '../app.css'
+  import '../prism.css'
   import { siteTitle } from '$lib/info.js';
   import Header from './Header.svelte';
   import Footer from './Footer.svelte';
-		import './styles.css';
 </script>
 
 <svelte:head>
@@ -10,8 +11,6 @@
   <meta property="og:type" content="website" />
   <meta name="robots" content="index, follow, max-image-preview:large, max-snippet:-1" />
   <meta property="og:locale" content="en_US" />
-  <link href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/1.0.0/tailwind.min.css" rel="stylesheet" />
-  <link href="https://cdnjs.cloudflare.com/ajax/libs/sveltekit-embed/0.0.12/sveltekit-embed.min.css" rel="stylesheet" />
 </svelte:head>
 
 <Header />
--- site/htdocs/svelte.config.js
+++ site/htdocs/svelte.config.js
@@ -1,14 +1,13 @@
-import { mdsvex } from 'mdsvex'
-import mdsvexConfig from './mdsvex.config.js'
-import preprocess from 'svelte-preprocess'
-import adapter from '@sveltejs/adapter-vercel'
+import adapter from '@sveltejs/adapter-vercel';
+import { mdsvex } from 'mdsvex';
+import mdsvexConfig from './mdsvex.config.js';
+import preprocess from 'svelte-preprocess';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   extensions: ['.svelte', ...mdsvexConfig.extensions],
 
-  // Consult https://github.com/sveltejs/svelte-preprocess
-  // for more information about preprocessors
   preprocess: [
     preprocess({
       postcss: true
@@ -19,11 +18,10 @@ const config = {
   kit: {
     adapter: adapter(),
 
-    // remove this if you don't want prerendering
     prerender: {
       entries: ['*', '/sitemap.xml', '/rss.xml']
     }
   }
-}
+};
 
-export default config
+export default config;
