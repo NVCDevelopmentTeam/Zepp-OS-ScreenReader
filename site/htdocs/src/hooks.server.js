e0b04811becea7139b2c74b372ef378661bd4dce
NVCDevelopmentTeam <82768878+NVCDevelopmentTeam@users.noreply.github.com>
2024-11-06 09:12:56 +0700

    update code

---
+++ site/htdocs/src/hooks.server.js
@@ -0,0 +1,9 @@
+import SvelteKitAuth from "@auth/sveltekit";
+import GitHub from "@auth/core/providers/github";
+import { GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET } from "$env/static/private";
+
+export const handle = SvelteKitAuth({
+  providers: [
+    GitHub({ clientId: GITHUB_CLIENT_ID, clientSecret: GITHUB_CLIENT_SECRET })
+  ]
+});
