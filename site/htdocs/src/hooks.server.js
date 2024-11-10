8f354ea953e74ae9c363d839b02639bc29a7636f
NVCDevelopmentTeam <82768878+NVCDevelopmentTeam@users.noreply.github.com>
2024-11-10 09:55:09 +0700

    update code

--- site/htdocs/src/hooks.server.js
+++ site/htdocs/src/hooks.server.js
@@ -1,6 +1,5 @@
-import { SvelteKitAuth } from "@auth/sveltekit"
-import GitHub from "@auth/sveltekit/providers/github"
-import dotenv from 'dotenv';
+import { SvelteKitAuth } from "@auth/sveltekit";
+import GitHub from "@auth/sveltekit/providers/github";
 import { GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET } from "$env/static/private";
 
 export const { handle, signIn, signOut } = SvelteKitAuth({
