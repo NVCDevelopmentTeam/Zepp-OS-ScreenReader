a6aaa74c8a317633af230c5538bcebde162d5eb2
NVCDevelopmentTeam <82768878+NVCDevelopmentTeam@users.noreply.github.com>
2024-11-14 11:11:53 +0700

    fix

--- site/htdocs/src/routes/contact/+page.svelte
+++ site/htdocs/src/routes/contact/+page.svelte
@@ -12,8 +12,8 @@ import Contact from '$lib/components/Contact.svelte';
 <p>
 	If you have any questions, feedback, or suggestions about ZSR, please feel free to contact me. You can fill out the form below, or send me an email at contact@Zsr.net. I will try to reply as soon as possible. Thank you for using ZSR and supporting our project!
 </p>
+<hr/>
 <h2>Contact form</h2>
 <p>Please fill in your information in the form below <br> (Fields marked * are required)</p>
-<hr/>
 <Contact />
 </div>
