d791c914c513b505a67ca9d53efee5c9b1986cbf
NVCDevelopmentTeam <Quang.hnm.hd.2k@gmail.com>
2024-11-24 09:05:33 +0700

    add oauth

---
+++ site/README.md
@@ -0,0 +1,92 @@
+# Sveltia CMS Authenticator
+
+This simple [Cloudflare Workers](https://workers.cloudflare.com/) script allows [Sveltia CMS](https://github.com/sveltia/sveltia-cms) (or Netlify/Decap CMS) users to authenticate with [GitHub](https://docs.github.com/en/apps/oauth-apps/building-oauth-apps/authorizing-oauth-apps) or [GitLab](https://docs.gitlab.com/ee/api/oauth2.html#authorization-code-flow).
+
+You don’t have to use it if you previously had Netlify/Decap CMS and your site is still being deployed to Netlify or if you have already used [another 3rd party OAuth client](https://decapcms.org/docs/external-oauth-clients/).
+
+You can use it if your site is hosted (or has been moved to) somewhere else, such as [Cloudflare Pages](https://pages.cloudflare.com/) or [GitHub Pages](https://pages.github.com/), and you don’t have any other 3rd party client yet.
+
+## How to use it
+
+### Step 1. Deploy this project to Cloudflare Workers
+
+Sign up with Cloudflare, and click the button below to start deploying.
+
+[![Deploy to Cloudflare Workers](https://deploy.workers.cloudflare.com/button)](https://deploy.workers.cloudflare.com/?url=https://github.com/sveltia/sveltia-cms-auth)
+
+Alternatively, you can clone the project and run [`wrangler deploy`](https://developers.cloudflare.com/workers/wrangler/commands/#deploy) locally.
+
+Once deployed, open your Cloudflare Workers dashboard, select the `sveltia-cms-auth` service, then the worker URL (`https://sveltia-cms-auth.<SUBDOMAIN>.workers.dev`) will be displayed. Copy it for Step 2. It will also be used in Step 4.
+
+### Step 2. Register the Worker as an OAuth app
+
+#### GitHub
+
+[Register a new OAuth application](https://github.com/settings/applications/new) on GitHub ([details](https://docs.github.com/en/apps/oauth-apps/building-oauth-apps/creating-an-oauth-app)) with the following properties, including your Worker URL from Step 1:
+
+- Application name: `Sveltia CMS Authenticator` (or whatever)
+- Homepage URL: `https://github.com/sveltia/sveltia-cms-auth` (or whatever)
+- Application description: (can be left empty)
+- Authorization callback URL: `<YOUR_WORKER_URL>/callback`
+
+Once registered, click on the **Generate a new client secret** button. The app’s **Client ID** and **Client Secret** will be displayed. We’ll use them in Step 3 below.
+
+#### GitLab
+
+[Register a new OAuth application](https://gitlab.com/-/user_settings/applications) on GitLab ([details](https://docs.gitlab.com/ee/integration/oauth_provider.html#create-a-user-owned-application)) with the following properties, including your Worker URL from Step 1:
+
+- Name: `Sveltia CMS Authenticator` (or whatever)
+- Redirect URI: `<YOUR_WORKER_URL>/callback`
+- Confidential: Yes
+- Scopes: `api` only
+
+Once registered, the app’s **Application ID** and **Secret** will be displayed. We’ll use them in Step 3 below.
+
+### Step 3. Configure the Worker
+
+Go back to the `sveltia-cms-auth` service page on the Cloudflare dashboard, select **Settings** > **Variables**, and add the following Environment Variables to your worker ([details](https://developers.cloudflare.com/workers/platform/environment-variables/#environment-variables-via-the-dashboard)):
+
+#### GitHub
+
+- `GITHUB_CLIENT_ID`: **Client ID** from Step 2
+- `GITHUB_CLIENT_SECRET`: **Client Secret** from Step 2; click the **Encrypt** button to hide it
+- `GITHUB_HOSTNAME`: Required only if you’re using GitHub Enterprise Server. Default: `github.com`
+
+#### GitLab
+
+- `GITLAB_CLIENT_ID`: **Application ID** from Step 2
+- `GITLAB_CLIENT_SECRET`: **Secret** from Step 2; click the **Encrypt** button to hide it
+- `GITLAB_HOSTNAME`: Required only if you’re using a self-hosted instance. Default: `gitlab.com`
+
+#### Both GitHub and GitLab
+
+- `ALLOWED_DOMAINS`: (Optional) Your site’s hostname, e.g. `www.example.com`
+  - Multiple hostnames can be defined as a comma-separated list, e.g. `www.example.com, www.example.org`
+  - A wildcard (`*`) can be used to match any subdomain, e.g. `*.example.com` that will match `www.example.com`, `blog.example.com`, `docs.api.example.com`, etc. (but not `example.com`)
+  - To match a `www`-less naked domain and all the subdomains, use `example.com, *.example.com`
+
+Save and deploy.
+
+### Step 4. Update your CMS configuration
+
+Open `admin/config.yml` locally or remotely, and add your Worker URL from Step 1 as the new `base_url` property under `backend`:
+
+```diff
+ backend:
+   name: github # or gitlab
+   repo: username/repo
+   branch: main
++  base_url: <YOUR_WORKER_URL>
+```
+
+Commit the change. Once deployed, you can sign into Sveltia CMS remotely with GitHub or GitLab!
+
+## FAQ
+
+### Why do I have to set this thing up in the first place?
+
+Technically, we could host Sveltia CMS Authenticator on our own server and let anyone use it, just like Netlify does. The cost probably wouldn’t matter because it’s just a small, short-lived script. However, running such a **service** certainly comes with legal, privacy and security liabilities that we cannot afford. Remember that Sveltia CMS is nothing more than [@kyoshino](https://github.com/kyoshino)’s personal project. That’s why the authenticator is not offered as SaaS and you have to install it yourself.
+
+## Acknowledgements
+
+This project was inspired by [`netlify-cms-oauth-firebase`](https://github.com/Herohtar/netlify-cms-oauth-firebase).
---
+++ site/src/index.js
@@ -0,0 +1,302 @@
+/**
+ * List of supported OAuth providers.
+ */
+const supportedProviders = ['github', 'gitlab'];
+/**
+ * Escape the given string for safe use in a regular expression.
+ * @param {string} str - Original string.
+ * @returns {string} Escaped string.
+ * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_expressions#escaping
+ */
+const escapeRegExp = (str) => str.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+
+/**
+ * Output HTML response that communicates with the window opener.
+ * @param {object} args - Options.
+ * @param {string} [args.provider] - Backend name, e,g. `github`.
+ * @param {string} [args.token] - OAuth token.
+ * @param {string} [args.error] - Error message when an OAuth token is not available.
+ * @param {string} [args.errorCode] - Error code to be used to localize the error message in
+ * Sveltia CMS.
+ * @returns {Response} Response with HTML.
+ */
+const outputHTML = ({ provider = 'unknown', token, error, errorCode }) => {
+  const state = error ? 'error' : 'success';
+  const content = error ? { provider, error, errorCode } : { provider, token };
+
+  return new Response(
+    `
+      <!doctype html><html><body><script>
+        (() => {
+          window.addEventListener('message', ({ data, origin }) => {
+            if (data === 'authorizing:${provider}') {
+              window.opener?.postMessage(
+                'authorization:${provider}:${state}:${JSON.stringify(content)}',
+                origin
+              );
+            }
+          });
+          window.opener?.postMessage('authorizing:${provider}', '*');
+        })();
+      </script></body></html>
+    `,
+    {
+      headers: {
+        'Content-Type': 'text/html;charset=UTF-8',
+        // Delete CSRF token
+        'Set-Cookie': `csrf-token=deleted; HttpOnly; Max-Age=0; Path=/; SameSite=Lax; Secure`,
+      },
+    },
+  );
+};
+
+/**
+ * Handle the `auth` method, which is the first request in the authorization flow.
+ * @param {Request} request - HTTP request.
+ * @param {{ [key: string]: string }} env - Environment variables.
+ * @returns {Promise<Response>} HTTP response.
+ */
+const handleAuth = async (request, env) => {
+  const { url } = request;
+  const { origin, searchParams } = new URL(url);
+  const { provider, site_id: domain } = Object.fromEntries(searchParams);
+
+  if (!provider || !supportedProviders.includes(provider)) {
+    return outputHTML({
+      error: 'Your Git backend is not supported by the authenticator.',
+      errorCode: 'UNSUPPORTED_BACKEND',
+    });
+  }
+
+  const {
+    ALLOWED_DOMAINS,
+    GITHUB_CLIENT_ID,
+    GITHUB_CLIENT_SECRET,
+    GITHUB_HOSTNAME = 'github.com',
+    GITLAB_CLIENT_ID,
+    GITLAB_CLIENT_SECRET,
+    GITLAB_HOSTNAME = 'gitlab.com',
+  } = env;
+
+  // Check if the domain is whitelisted
+  if (
+    ALLOWED_DOMAINS &&
+    !ALLOWED_DOMAINS.split(/,/).some((str) =>
+      // Escape the input, then replace a wildcard for regex
+      (domain ?? '').match(new RegExp(`^${escapeRegExp(str.trim()).replace('\\*', '.+')}$`)),
+    )
+  ) {
+    return outputHTML({
+      provider,
+      error: 'Your domain is not allowed to use the authenticator.',
+      errorCode: 'UNSUPPORTED_DOMAIN',
+    });
+  }
+
+  // Generate a random string for CSRF protection
+  const csrfToken = globalThis.crypto.randomUUID().replaceAll('-', '');
+  let authURL = '';
+
+  // GitHub
+  if (provider === 'github') {
+    if (!GITHUB_CLIENT_ID || !GITHUB_CLIENT_SECRET) {
+      return outputHTML({
+        provider,
+        error: 'OAuth app client ID or secret is not configured.',
+        errorCode: 'MISCONFIGURED_CLIENT',
+      });
+    }
+
+    const params = new URLSearchParams({
+      client_id: GITHUB_CLIENT_ID,
+      scope: 'repo,user',
+      state: csrfToken,
+    });
+
+    authURL = `https://${GITHUB_HOSTNAME}/login/oauth/authorize?${params.toString()}`;
+  }
+
+  // GitLab
+  if (provider === 'gitlab') {
+    if (!GITLAB_CLIENT_ID || !GITLAB_CLIENT_SECRET) {
+      return outputHTML({
+        provider,
+        error: 'OAuth app client ID or secret is not configured.',
+        errorCode: 'MISCONFIGURED_CLIENT',
+      });
+    }
+
+    const params = new URLSearchParams({
+      client_id: GITLAB_CLIENT_ID,
+      redirect_uri: `${origin}/callback`,
+      response_type: 'code',
+      scope: 'api',
+      state: csrfToken,
+    });
+
+    authURL = `https://${GITLAB_HOSTNAME}/oauth/authorize?${params.toString()}`;
+  }
+
+  // Redirect to the authorization server
+  return new Response('', {
+    status: 302,
+    headers: {
+      Location: authURL,
+      // Cookie expires in 10 minutes; Use `SameSite=Lax` to make sure the cookie is sent by the
+      // browser after redirect
+      'Set-Cookie':
+        `csrf-token=${provider}_${csrfToken}; ` +
+        `HttpOnly; Path=/; Max-Age=600; SameSite=Lax; Secure`,
+    },
+  });
+};
+
+/**
+ * Handle the `callback` method, which is the second request in the authorization flow.
+ * @param {Request} request - HTTP request.
+ * @param {{ [key: string]: string }} env - Environment variables.
+ * @returns {Promise<Response>} HTTP response.
+ */
+const handleCallback = async (request, env) => {
+  const { url, headers } = request;
+  const { origin, searchParams } = new URL(url);
+  const { code, state } = Object.fromEntries(searchParams);
+
+  const [, provider, csrfToken] =
+    headers.get('Cookie')?.match(/\bcsrf-token=([a-z-]+?)_([0-9a-f]{32})\b/) ?? [];
+
+  if (!provider || !supportedProviders.includes(provider)) {
+    return outputHTML({
+      error: 'Your Git backend is not supported by the authenticator.',
+      errorCode: 'UNSUPPORTED_BACKEND',
+    });
+  }
+
+  if (!code || !state) {
+    return outputHTML({
+      provider,
+      error: 'Failed to receive an authorization code. Please try again later.',
+      errorCode: 'AUTH_CODE_REQUEST_FAILED',
+    });
+  }
+
+  if (!csrfToken || state !== csrfToken) {
+    return outputHTML({
+      provider,
+      error: 'Potential CSRF attack detected. Authentication flow aborted.',
+      errorCode: 'CSRF_DETECTED',
+    });
+  }
+
+  const {
+    GITHUB_CLIENT_ID,
+    GITHUB_CLIENT_SECRET,
+    GITHUB_HOSTNAME = 'github.com',
+    GITLAB_CLIENT_ID,
+    GITLAB_CLIENT_SECRET,
+    GITLAB_HOSTNAME = 'gitlab.com',
+  } = env;
+
+  let tokenURL = '';
+  let requestBody = {};
+
+  // GitHub
+  if (provider === 'github') {
+    if (!GITHUB_CLIENT_ID || !GITHUB_CLIENT_SECRET) {
+      return outputHTML({
+        provider,
+        error: 'OAuth app client ID or secret is not configured.',
+        errorCode: 'MISCONFIGURED_CLIENT',
+      });
+    }
+
+    tokenURL = `https://${GITHUB_HOSTNAME}/login/oauth/access_token`;
+    requestBody = {
+      code,
+      client_id: GITHUB_CLIENT_ID,
+      client_secret: GITHUB_CLIENT_SECRET,
+    };
+  }
+
+  if (provider === 'gitlab') {
+    if (!GITLAB_CLIENT_ID || !GITLAB_CLIENT_SECRET) {
+      return outputHTML({
+        provider,
+        error: 'OAuth app client ID or secret is not configured.',
+        errorCode: 'MISCONFIGURED_CLIENT',
+      });
+    }
+
+    tokenURL = `https://${GITLAB_HOSTNAME}/oauth/token`;
+    requestBody = {
+      code,
+      client_id: GITLAB_CLIENT_ID,
+      client_secret: GITLAB_CLIENT_SECRET,
+      grant_type: 'authorization_code',
+      redirect_uri: `${origin}/callback`,
+    };
+  }
+
+  let response;
+  let token = '';
+  let error = '';
+
+  try {
+    response = await fetch(tokenURL, {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(requestBody),
+    });
+  } catch {
+    //
+  }
+
+  if (!response) {
+    return outputHTML({
+      provider,
+      error: 'Failed to request an access token. Please try again later.',
+      errorCode: 'TOKEN_REQUEST_FAILED',
+    });
+  }
+
+  try {
+    ({ access_token: token, error } = await response.json());
+  } catch {
+    return outputHTML({
+      provider,
+      error: 'Server responded with malformed data. Please try again later.',
+      errorCode: 'MALFORMED_RESPONSE',
+    });
+  }
+
+  return outputHTML({ provider, token, error });
+};
+
+export default {
+  /**
+   * The main request handler.
+   * @param {Request} request - HTTP request.
+   * @param {{ [key: string]: string }} env - Environment variables.
+   * @returns {Promise<Response>} HTTP response.
+   * @see https://developers.cloudflare.com/workers/runtime-apis/fetch/
+   * @see https://docs.github.com/en/apps/oauth-apps/building-oauth-apps/authorizing-oauth-apps
+   * @see https://docs.gitlab.com/ee/api/oauth2.html#authorization-code-flow
+   */
+  async fetch(request, env) {
+    const { method, url } = request;
+    const { pathname } = new URL(url);
+
+    if (method === 'GET' && ['/auth', '/oauth/authorize'].includes(pathname)) {
+      return handleAuth(request, env);
+    }
+
+    if (method === 'GET' && ['/callback', '/oauth/redirect'].includes(pathname)) {
+      return handleCallback(request, env);
+    }
+
+    return new Response('', { status: 404 });
+  },
+};
