eb60abb17321caa685c77d6f9ef82d546aac4ee3
NVCDevelopmentTeam <Quang.hnm.hd.2k@gmail.com>
2024-12-27 08:40:01 +0700

    update packages

--- site/htdocs/src/routes/[admin]/+page.js
+++ site/htdocs/src/routes/[admin]/+page.js
@@ -1,37 +1,9 @@
-// src/routes/[admin]/+page.js
-
-// Disable prerendering, enable client-side rendering
 export const prerender = false;
-export const ssr = false;
-export const csr = true;
 
-import { onMount } from 'svelte';
-import { init } from '@sveltia/cms';
+import config from '../../../sveltia.config.js';
 
-// Load configuration from sveltia.config.js
-export const load = async ({ fetch }) => {
-  try {
-    const res = await fetch('/sveltia.config.js');
-    const text = await res.text();
-    const module = new Function('export default ' + text)(); 
-    const config = module.default; 
-
-    return {
-      props: { config }
-    };
-  } catch (error) {
-    console.error('Error loading configuration:', error);
-    return {
-      props: { config: null } 
-    };
-  }
+export const load = async () => {
+  return {
+    config
+  };
 };
-
-// Initialize CMS on client-side only
-onMount(() => {
-  if (typeof window !== 'undefined' && config) {
-    init({
-      config
-    });
-  }
-});
