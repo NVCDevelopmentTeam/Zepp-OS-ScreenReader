844d59a0d0cd09fb91a711d3b6b1226703bc4aeb
NVCDevelopmentTeam <Quang.hnm.hd.2k@gmail.com>
2024-12-27 10:17:34 +0700

    update code

--- site/htdocs/src/routes/[admin]/+page.js
+++ site/htdocs/src/routes/[admin]/+page.js
@@ -1,7 +1,5 @@
 export const prerender = false;
 
-import config from '../../../sveltia.config.js';
-
 export const load = async () => {
   return {
     config
--- site/htdocs/svelte.config.js
+++ site/htdocs/svelte.config.js
@@ -1,30 +1,22 @@
 import adapter from '@sveltejs/adapter-vercel';
 import { mdsvex } from 'mdsvex';
 import mdsvexConfig from './mdsvex.config.js';
-import preprocess from 'svelte-preprocess';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import sveltia from '@sveltia/cms';
+import sveltiaConfig from './sveltia.config.js';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   extensions: ['.svelte', '.md'],
 
-  // Preprocess configuration
   preprocess: [
-    vitePreprocess(),
-    preprocess({
-      postcss: true
-    }),
-    mdsvex({
-      extensions: ['.md'],
-      ...mdsvexConfig
-    })
+    vitePreprocess({ script: true }),
+    mdsvex(mdsvexConfig),
+    sveltia(sveltiaConfig)
   ],
 
   kit: {
-    // Adapter configuration
     adapter: adapter(),
-
-    // Prerender configuration
     prerender: {
       entries: ['*', '/sitemap.xml', '/rss.xml'],
       handleMissingId: 'warn'
