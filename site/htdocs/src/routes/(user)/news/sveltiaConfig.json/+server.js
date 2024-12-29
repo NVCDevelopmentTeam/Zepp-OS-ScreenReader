2ac3b0768595b383e1030e07523db896473402f0
NVCDevelopmentTeam <Quang.hnm.hd.2k@gmail.com>
2024-12-29 10:25:35 +0700

    update code

---
+++ site/htdocs/src/routes/(user)/news/sveltiaConfig.json/+server.js
@@ -0,0 +1,20 @@
+import { json } from '@sveltejs/kit';
+import fs from 'fs';
+import path from 'path';
+
+export async function GET() {
+  const filePath = path.resolve('C:/Users/DELL/Documents/GitHub/Zepp-OS-ScreenReader/site/htdocs/sveltiaConfig.json');
+
+  try {
+    const data = fs.readFileSync(filePath, 'utf8');
+    const jsonObject = JSON.parse(data);
+    const jsCode = `export const config = ${JSON.stringify(jsonObject, null, 2)};`;
+    return new Response(jsCode, {
+      headers: {
+        'Content-Type': 'application/javascript',
+      },
+    });
+  } catch (error) {
+    return new Response('Lỗi khi đọc hoặc phân tích cú pháp tệp JSON', { status: 500 });
+  }
+}
