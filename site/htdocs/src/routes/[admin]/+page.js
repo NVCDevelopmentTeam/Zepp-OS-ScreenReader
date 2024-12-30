4a300942a0e4f032f3ba66ab365e474f3ba1a407
NVCDevelopmentTeam <Quang.hnm.hd.2k@gmail.com>
2024-12-30 10:15:33 +0700

    fixbug

--- site/htdocs/src/routes/[admin]/+page.js
+++ site/htdocs/src/routes/[admin]/+page.js
@@ -1,5 +1,5 @@
-export const ssr = false;
-export const prerender = true;
+export const ssr = true;
+export const prerender = false;
 import sveltiaConfig from '../../../sveltiaconfig.json';
 
 export const load = async () => {
