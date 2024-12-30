e2cf01dced40574f51ad00e9ff35c4a3e8947b1d
NVCDevelopmentTeam <Quang.hnm.hd.2k@gmail.com>
2024-12-30 08:04:11 +0700

    update code

--- site/htdocs/svelte.config.js
+++ site/htdocs/svelte.config.js
@@ -1,7 +1,7 @@
 import { mdsvex } from 'mdsvex'
 import mdsvexConfig from './mdsvex.config.js'
 import preprocess from 'svelte-preprocess'
-import adapter from '@sveltejs/adapter-static'
+import adapter from '@sveltejs/adapter-vercel'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
