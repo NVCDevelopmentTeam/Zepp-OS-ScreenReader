5e2caa15431bff8e21e80944a52c7c313ababdea
NVCDevelopmentTeam <Quang.hnm.hd.2k@gmail.com>
2025-01-01 08:53:13 +0700

    update code

--- site/htdocs/src/routes/(user)/news/sveltiaconfig.json/+server.js
+++ site/htdocs/src/routes/(user)/news/sveltiaconfig.json/+server.js
@@ -2,20 +2,27 @@ import { json } from '@sveltejs/kit';
 import fs from 'fs';
 import path from 'path';
 
-export async function GET() {
+export async function GET({ url }) {
     try {
-        // Access the file from the static directory
-        const filePath = path.join(process.cwd(), 'static', 'sveltiaconfig.json');
-        
-        // Read the file synchronously
-        const fileContent = fs.readFileSync(filePath, 'utf-8');
-        
-        // Parse and return the JSON data
+        let fileContent;
+
+        // Determine the environment to decide how to read the file
+        if (process.env.VERCEL || process.env.NODE_ENV === 'production') {
+            // Fetch the file from the URL when deployed
+            const postRes = await fetch(`${url.origin}/sveltiaconfig.json`);
+            fileContent = await postRes.text();
+        } else {
+            // Read the file locally when running on localhost
+            const filePath = path.join(process.cwd(), 'static', 'sveltiaconfig.json');
+            fileContent = fs.readFileSync(filePath, 'utf-8');
+        }
+
+        // Parse the JSON content
         const sveltiaConfig = JSON.parse(fileContent);
         return json(sveltiaConfig);
+
     } catch (error) {
         console.error('Error reading JSON file:', error);
-        console.error('File path attempted:', path.join(process.cwd(), 'static', 'sveltiaconfig.json'));
         return new Response('Error reading JSON file', { status: 500 });
     }
 }
