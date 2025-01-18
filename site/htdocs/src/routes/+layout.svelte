ad09f58e1af2ea008fa79b3c8a3c0194b0b9533d
NVCDevelopmentTeam <Quang.hnm.hd.2k@gmail.com>
2025-01-18 19:50:48 +0700

    update code

--- site/htdocs/src/routes/+layout.svelte
+++ site/htdocs/src/routes/+layout.svelte
@@ -5,6 +5,8 @@
 </script>
 
 <svelte:head>
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3602487920405886"
+     crossorigin="anonymous"></script>
   <title>{siteTitle}</title>
     <meta name="google-adsense-account" content="ca-pub-3602487920405886" />
 <link href="/sveltiaconfig.json" type="application/json" rel="cms-config-url" />
