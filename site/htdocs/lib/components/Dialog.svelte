36e41b9db6b178cdb4af0d6badc2aa1bd6e09b70
NVCDevelopmentTeam <Quang.hnm.hd.2k@gmail.com>
2025-02-22 23:03:07 +0700

    update code

--- site/htdocs/lib/components/Dialog.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-	import { createBubbler } from 'svelte/legacy';
-
-	const bubble = createBubbler();
-	let { dialog = $bindable(), children } = $props();
-</script>
-<dialog bind:this={dialog} onclose={bubble('close')}>
-	{@render children?.()}
-</dialog>
