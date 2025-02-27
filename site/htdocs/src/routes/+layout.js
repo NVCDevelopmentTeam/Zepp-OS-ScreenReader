12fe9ef2c5aed365635c42661c4d7821a732199b
NVCDevelopmentTeam <Quang.hnm.hd.2k@gmail.com>
2025-02-27 18:25:25 +0700

    changes layout

--- site/htdocs/src/routes/+layout.js
+++ site/htdocs/src/routes/+layout.js
@@ -1,2 +1,10 @@
 // remove this if you do not want your pages to be prerendered
-export const prerender = true
+import { dev } from '$app/environment';
+import { inject } from '@vercel/analytics';
+ 
+inject({ mode: dev ? 'development' : 'production' });
+export const prerender  = true;
+
+import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
+
+injectSpeedInsights();
