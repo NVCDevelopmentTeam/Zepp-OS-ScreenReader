b28a64d9774c9d0d6d08d2bb4820ea60638bea48
NVCDevelopmentTeam <Quang.hnm.hd.2k@gmail.com>
2025-02-27 11:52:53 +0700

    update components

--- site/htdocs/src/lib/components/Analytics.svelte
+++ site/htdocs/src/lib/components/Analytics.svelte
@@ -1,33 +1,26 @@
 <script>
-  import { onMount } from 'svelte';
   import { page } from '$app/stores';
   import { browser } from '$app/environment';
 
-  const trackingId = 'G-4YCKS1JTX8'; 
+  const trackingId = 'G-4YCKS1JTX8'; // Thay bằng mã đo lường của anh
 
-  function gtag() {
-    window.dataLayer.push(arguments);
-  }
-
-  onMount(() => {
-    if (browser) {
-      window.dataLayer = window.dataLayer || [];
-      gtag('js', new Date());
-      gtag('config', trackingId, {
-        page_path: $page.url.pathname,
-        page_title: document.title,
-      });
-    }
-  });
-
-  $: if (browser && typeof gtag === 'function') {
+  // Theo dõi thay đổi đường dẫn
+  $: if (browser && typeof gtag !== 'undefined') {
     gtag('config', trackingId, {
-      page_path: $page.url.pathname,
       page_title: document.title,
+      page_path: $page.url.pathname,
     });
   }
 </script>
 
 <svelte:head>
   <script async src={`https://www.googletagmanager.com/gtag/js?id=${trackingId}`}></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag() {
+      dataLayer.push(arguments);
+    }
+    gtag('js', new Date());
+    gtag('config', '${trackingId}');
+  </script>
 </svelte:head>
