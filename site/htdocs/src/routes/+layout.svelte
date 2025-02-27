bfb51fa0a7b024c6f4fbae69e68db5c5e41a396e
NVCDevelopmentTeam <Quang.hnm.hd.2k@gmail.com>
2025-02-27 18:59:29 +0700

    Remove Meta

--- site/htdocs/src/routes/+layout.svelte
+++ site/htdocs/src/routes/+layout.svelte
@@ -16,8 +16,6 @@
   <meta name="google-adsense-account" content="ca-pub-3602487920405886" />
   <link href="/sveltiaconfig.json" type="application/json" rel="cms-config-url" />
   <meta property="og:type" content="website" />
-  <meta name="robots" content="index, follow, max-image-preview:large, max-snippet:-1" />
-  <meta name="theme-color" media="(prefers-color-scheme: light)" content="#FFFFFF" />
 </svelte:head>
 <main>
 {@render children?.()}
