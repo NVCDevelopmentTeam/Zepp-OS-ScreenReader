c55dec3fb9b35a88da3be4e9aea868398812e43c
NVCDevelopmentTeam <Quang.hnm.hd.2k@gmail.com>
2025-03-03 07:01:12 +0700

    update packages

--- site/htdocs/src/lib/components/Adsense.svelte
+++ site/htdocs/src/lib/components/Adsense.svelte
@@ -11,6 +11,6 @@
   }
 </script>
 <svelte:head>
-<script defer src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3602487920405886"
+<script defer  src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3602487920405886"
      crossorigin="anonymous"></script>
 </svelte:head>
