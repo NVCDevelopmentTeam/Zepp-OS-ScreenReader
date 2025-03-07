5dae9c5727d775cd14db1819b4f3e1f8b54ff702
NVCDevelopmentTeam <Quang.hnm.hd.2k@gmail.com>
2025-03-07 08:44:39 +0700

    fixbug

--- site/htdocs/svelte.config.js
+++ site/htdocs/svelte.config.js
@@ -1,7 +1,7 @@
 import { mdsvex } from 'mdsvex'
 import mdsvexConfig from './mdsvex.config.js'
 import preprocess from 'svelte-preprocess'
-import adapter from '@sveltejs/adapter-cloudflare-workers';
+import adapter from '@sveltejs/adapter-cloudflare';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
