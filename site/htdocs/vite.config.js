7104ea68320cd12bd58003369d602a3bc19273dc
Coding Nguyen <Quang.hnm.hd.2k@gmail.com>
2025-08-31 12:16:31 +0700

    Add @tailwindcss/vite and update dependencies
    Introduces @tailwindcss/vite to the project and updates related configuration in vite.config.js to include the plugin. Also bumps versions for @sveltejs/kit, svelte, and @sveltia/cms for improved compatibility and latest features.

--- site/htdocs/vite.config.js
+++ site/htdocs/vite.config.js
@@ -1,9 +1,10 @@
 import { sveltekit } from '@sveltejs/kit/vite'
 import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite';
 import sveltiaCms from 'astro-sveltia-cms'
 
 export default defineConfig({
-  plugins: [sveltekit(), sveltiaCms()],
+  plugins: [sveltekit(), tailwindcss(), sveltiaCms()],
   server: {
     fs: {
       allow: ['.']
